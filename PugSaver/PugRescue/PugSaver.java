df7a2512eb75775a59a2841d64c60365d8885cac
Jake-Eff <116287071+Jake-Eff@users.noreply.github.com>
2025-08-27 13:31:55 -0700

    End of class aug 27

--- PugSaver/PugRescue/PugSaver.java
+++ PugSaver/PugRescue/PugSaver.java
@@ -3,15 +3,23 @@ import java.util.Objects;
 
 public class PugSaver {
 
-	//Moves every dog whose breed is "Golden" in the list to the back of the list
+	// Moves every dog whose breed is "Golden" in the list to the back of the list
 	public static void rescueGoldens(ArrayList<Dog> list) {
-		ArrayList<Dog> Saved = new ArrayList<Dog>();
+		// ArrayList<Dog> Saved = new ArrayList<Dog>();
+		// ArrayList<Dog> Notsaved = new ArrayList<Dog>();
 		for (int i = 0; i < list.size(); i++) {
-			if(list.get(i).getBreed().toLowerCase().contains("golden")){
-				Saved.add(list.get(i));
-				list.remove(i);
+			if (list.get(i).getBreed().toLowerCase().contains("golden")) {
+				for (int j = list.size(); j > 0; j--) {
+					if (!(list.get(i).getBreed().toLowerCase().contains("golden"))) {
+						Dog temp = list.get(j);
+						list.set(j, list.get(i));
+						list.set(i, temp);
+					} else {
+						j--;
+					}
+				}
 			}
 		}
-		
+
 	}
 }
