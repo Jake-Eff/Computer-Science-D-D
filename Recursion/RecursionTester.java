ceffd2f1cebc28bffc876b28ad07bcc04de2e03e
Jake-Eff <116287071+Jake-Eff@users.noreply.github.com>
2025-11-06 11:12:51 -0800

    nov 6

--- Recursion/RecursionTester.java
+++ Recursion/RecursionTester.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class RecursionTester {
     public static void main(String[] args) {
     
-        String str1 = "cd";
-        String str2 = "bcd";
-        Recursion.printPermutations(str1);
-        Recursion.solveHanoi(4);
+        // String str1 = "cd";
+        // String str2 = "bcd";
+        // Recursion.printPermutations(str1);
+        Recursion.solveHanoi(5);
     }
 }
