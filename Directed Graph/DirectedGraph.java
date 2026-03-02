1a11001a992750d19389bdd3f216512461f92b9f
Jake-Eff <116287071+Jake-Eff@users.noreply.github.com>
2026-03-02 09:17:34 -0800

    mar 2

---
+++ Directed Graph/DirectedGraph.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+public class DirectedGraph {
+    public static List<String> readAllLines(Path file) throws IOException {
+        try{
+
+        } catch (Exception e){
+            System.out.println("Couldn't read file");
+        }
+        Path p = Paths.get("relationships.txt");
+        return Files.readAllLines(p);
+    }
+}
+
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.util.ArrayList;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
+
+
+// public class DirectedGraph {
+
+
+//     public static ArrayList<String> readAllLines() {
+//        ArrayList<String> names = new ArrayList<String>();
+//         try {
+
+
+//         	  // Each element is one line from the file
+//         	  Path p = Paths.get(“NameOfYourFile.txt");
+//    return Files.readAllLines(p);
+//        } catch (Exception e) {
+// 	System.out.println(“Couldn’t read file”);
+//        }
+//       return names;
+//     }
+// }
+
