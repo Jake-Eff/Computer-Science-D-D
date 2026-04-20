6ecf8bd8f8f688e200643e23b9999bff187b436e
Jake-Eff <116287071+Jake-Eff@users.noreply.github.com>
2026-04-19 17:55:01 -0700

    apr 19

--- Huffman/HuffmanCodeGenerator.java
+++ Huffman/HuffmanCodeGenerator.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HuffmanCodeGenerator {
-    private HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    private HashMap<Character, Integer> map;
     public HuffmanCodeGenerator(String frequencyFile) {
         try (BufferedReader reader = new BufferedReader(new FileReader(frequencyFile))) {
+            map = new HashMap<Character, Integer>();
             int charAsInt;
-            ArrayList<Character> chars = new ArrayList<>();
             while ((charAsInt = reader.read()) != -1) {
                 char character = (char) charAsInt;
-                int value = map.get(character);
-                map.put(character, value + 1);
+                if(map.containsKey(character)){
+                    map.put(character, map.get(character) + 1);
+                } else{
+                    map.put(character, 1);
+                }
             }
             map.put(((char) 26), 1);
         } catch (Exception e) {
@@ -21,6 +24,9 @@ public class HuffmanCodeGenerator {
     }
 
     public int getFrequency(char c){
-        return map.get(c);
+        if(map.containsKey(c)){
+            return map.get(c);
+        }
+        return 0;
     }
 }
